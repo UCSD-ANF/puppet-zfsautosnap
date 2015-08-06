dd80e5996b3df34df01f6960efba888f8ac3ff85
Geoff Davis <gadavis@ucsd.edu>
2015-08-06 16:10:22 -0700

    comment out all of the zfsautosnap stuff

--- manifests/client.pp
+++ manifests/client.pp
@@ -22,30 +22,36 @@ class zfsautosnap::client (
   $client_username = 'zfssnap' # Created by the package installation
   $client_groupname = 'other'  # Specified by package
   $client_homedir = "/export/home/${client_username}"
-
-  $basefmri = 'svc:/system/filesystem/zfs/auto-snapshot'
-
-  $verbose_daily_real = $verbose_daily ? {
-    true  => 'true',
-    false => 'false',
+  $client_shell = $::osfamily ? {
+    'Solaris' => '/bin/pfsh',
+    'FreeBSD' => '/usr/local/bin/bash',
+    default   => '/bin/bash',
   }
 
-  $verbose_hourly_real = $verbose_hourly ? {
-    true  => 'true',
-    false => 'false',
-  }
+  # # Used by the old Solaris package only
+  # $basefmri = 'svc:/system/filesystem/zfs/auto-snapshot'
+
+  # $verbose_daily_real = $verbose_daily ? {
+  #   true  => 'true',
+  #   false => 'false',
+  # }
+
+  # $verbose_hourly_real = $verbose_hourly ? {
+  #   true  => 'true',
+  #   false => 'false',
+  # }
 
   ## Managed resources
-  package { 'IGPPzfsautosnap':
-    ensure  => 'installed',
-    require => Package['mbuffer'],
-  } ->
+  #  package { 'IGPPzfsautosnap':
+  #  ensure  => 'installed',
+  #  require => Package['mbuffer'],
+  #} ->
   user { $client_username :
     ensure   => 'role',
     uid      => '51',
     comment  => 'ZFS Automatic Snapshots role',
     gid      => 'other',
-    shell    => '/bin/pfsh',
+    shell    => $client_shell,
     home     => $client_homedir,
     password => 'NP',
   } ->
@@ -70,115 +76,115 @@ class zfsautosnap::client (
     mode   => '0700',
   }
 
-  file { '/usr/local/sbin/checkzfssnaplock':
-    ensure  => 'present',
-    source  => 'puppet:///modules/zfsautosnap/checkzfssnaplock',
-    owner   => 'root',
-    group   => 'sys',
-    mode    => '0755',
-    require => File['/usr/local/sbin'],
-  }
+  # file { '/usr/local/sbin/checkzfssnaplock':
+  #   ensure  => 'present',
+  #   source  => 'puppet:///modules/zfsautosnap/checkzfssnaplock',
+  #   owner   => 'root',
+  #   group   => 'sys',
+  #   mode    => '0755',
+  #   require => File['/usr/local/sbin'],
+  # }
 
-  file { '/usr/local/sbin/clearzfssnaplock':
-    ensure  => 'present',
-    source  => 'puppet:///modules/zfsautosnap/clearzfssnaplock',
-    owner   => 'root',
-    group   => 'sys',
-    mode    => '0755',
-    require => File['/usr/local/sbin'],
-  }
+  # file { '/usr/local/sbin/clearzfssnaplock':
+  #   ensure  => 'present',
+  #   source  => 'puppet:///modules/zfsautosnap/clearzfssnaplock',
+  #   owner   => 'root',
+  #   group   => 'sys',
+  #   mode    => '0755',
+  #   require => File['/usr/local/sbin'],
+  # }
 
-  cron { 'zfsautosnap daily lock checker':
-    ensure  => 'present',
-    user    => 'root',
-    minute  => 45,
-    hour    => 23,
-    command => join([
-      '/usr/local/sbin/checkzfssnaplock > /dev/null',
-      '[ $? = 2 ] && /usr/local/sbin/clearzfssnaplock',
-    ],'; '),
-    require => [
-      File['/usr/local/sbin/checkzfssnaplock'],
-      File['/usr/local/sbin/clearzfssnaplock']
-    ],
-  }
+  # cron { 'zfsautosnap daily lock checker':
+  #   ensure  => 'present',
+  #   user    => 'root',
+  #   minute  => 45,
+  #   hour    => 23,
+  #   command => join([
+  #     '/usr/local/sbin/checkzfssnaplock > /dev/null',
+  #     '[ $? = 2 ] && /usr/local/sbin/clearzfssnaplock',
+  #   ],'; '),
+  #   require => [
+  #     File['/usr/local/sbin/checkzfssnaplock'],
+  #     File['/usr/local/sbin/clearzfssnaplock']
+  #   ],
+  # }
 
-  file { '/usr/local/sbin/snaphourlykiller':
-    ensure  => 'present',
-    source  => 'puppet:///modules/zfsautosnap/snaphourlykiller',
-    owner   => 'root',
-    group   => 'sys',
-    mode    => '0755',
-    require => File['/usr/local/sbin'],
-  } ->
-  cron { 'snaphourlykiller':
-    ensure  => 'present',
-    command => '/usr/local/sbin/snaphourlykiller',
-    user    => 'root',
-    minute  => 15,
-  }
+  # file { '/usr/local/sbin/snaphourlykiller':
+  #   ensure  => 'present',
+  #   source  => 'puppet:///modules/zfsautosnap/snaphourlykiller',
+  #   owner   => 'root',
+  #   group   => 'sys',
+  #   mode    => '0755',
+  #   require => File['/usr/local/sbin'],
+  # } ->
+  # cron { 'snaphourlykiller':
+  #   ensure  => 'present',
+  #   command => '/usr/local/sbin/snaphourlykiller',
+  #   user    => 'root',
+  #   minute  => 15,
+  # }
 
-  svcprop { 'zfssnap daily type':
-    fmri     => "${basefmri}:daily",
-    property => 'zfs/backup',
-    value    => 'incremental_mbuffered',
-    require  => Package['IGPPzfsautosnap'],
-  }
+  # svcprop { 'zfssnap daily type':
+  #   fmri     => "${basefmri}:daily",
+  #   property => 'zfs/backup',
+  #   value    => 'incremental_mbuffered',
+  #   require  => Package['IGPPzfsautosnap'],
+  # }
 
-  svcprop { 'zfssnap daily host':
-    fmri     => "${basefmri}:daily",
-    property => 'zfs/backup-host',
-    value    => $target_hostname,
-    require  => Package['IGPPzfsautosnap'],
-  }
+  # svcprop { 'zfssnap daily host':
+  #   fmri     => "${basefmri}:daily",
+  #   property => 'zfs/backup-host',
+  #   value    => $target_hostname,
+  #   require  => Package['IGPPzfsautosnap'],
+  # }
 
-  svcprop { 'zfssnap daily zpool':
-    fmri     => "${basefmri}:daily",
-    property => 'zfs/backup-zpool',
-    value    => $target_pool,
-    require  => Package['IGPPzfsautosnap'],
-  }
+  # svcprop { 'zfssnap daily zpool':
+  #   fmri     => "${basefmri}:daily",
+  #   property => 'zfs/backup-zpool',
+  #   value    => $target_pool,
+  #   require  => Package['IGPPzfsautosnap'],
+  # }
 
-  svcprop { 'zfssnap daily user':
-    fmri     => "${basefmri}:daily",
-    property => 'zfs/backup-user',
-    value    => $target_username,
-    require  => Package['IGPPzfsautosnap'],
-  }
+  # svcprop { 'zfssnap daily user':
+  #   fmri     => "${basefmri}:daily",
+  #   property => 'zfs/backup-user',
+  #   value    => $target_username,
+  #   require  => Package['IGPPzfsautosnap'],
+  # }
 
-  service { "${basefmri}:daily" :
-    enable  => $enable_daily,
-    require => Package['IGPPzfsautosnap'],
-  } ->
-  svcprop { 'zfssnap daily verbose':
-    fmri     => "${basefmri}:daily",
-    property => 'zfs/verbose',
-    value    => $verbose_daily_real,
-  }
-  service { "${basefmri}:hourly" :
-    enable  => $enable_hourly,
-    require => Package['IGPPzfsautosnap'],
-  } ->
-  svcprop { 'zfssnap hourly verbose':
-    fmri     => "${basefmri}:hourly",
-    property => 'zfs/verbose',
-    value    => $verbose_hourly_real,
-  }
-  service { "${basefmri}:monthly" :
-    enable  => false,
-    require => Package['IGPPzfsautosnap'],
-  }
-  service { "${basefmri}:weekly" :
-    enable  => false,
-    require => Package['IGPPzfsautosnap'],
-  }
-  service { "${basefmri}:frequent" :
-    enable  => false,
-    require => Package['IGPPzfsautosnap'],
-  }
-  service { "${basefmri}:event" :
-    enable  => false,
-    require => Package['IGPPzfsautosnap'],
-  }
+  # service { "${basefmri}:daily" :
+  #   enable  => $enable_daily,
+  #   require => Package['IGPPzfsautosnap'],
+  # } ->
+  # svcprop { 'zfssnap daily verbose':
+  #   fmri     => "${basefmri}:daily",
+  #   property => 'zfs/verbose',
+  #   value    => $verbose_daily_real,
+  # }
+  # service { "${basefmri}:hourly" :
+  #   enable  => $enable_hourly,
+  #   require => Package['IGPPzfsautosnap'],
+  # } ->
+  # svcprop { 'zfssnap hourly verbose':
+  #   fmri     => "${basefmri}:hourly",
+  #   property => 'zfs/verbose',
+  #   value    => $verbose_hourly_real,
+  # }
+  # service { "${basefmri}:monthly" :
+  #   enable  => false,
+  #   require => Package['IGPPzfsautosnap'],
+  # }
+  # service { "${basefmri}:weekly" :
+  #   enable  => false,
+  #   require => Package['IGPPzfsautosnap'],
+  # }
+  # service { "${basefmri}:frequent" :
+  #   enable  => false,
+  #   require => Package['IGPPzfsautosnap'],
+  # }
+  # service { "${basefmri}:event" :
+  #   enable  => false,
+  #   require => Package['IGPPzfsautosnap'],
+  # }
 
 }
