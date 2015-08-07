e65964e0a710aefdca63effe2ae52fc2c46b097f
Geoff Davis <gadavis@ucsd.edu>
2015-08-06 17:37:42 -0700

    Create the target user
    In our production module, this was being done in the site class. Really, the
    service should be managing this.

--- manifests/server.pp
+++ manifests/server.pp
@@ -11,12 +11,45 @@ class zfsautosnap::server (
   $client_ssh_pubkey,
   $client_ssh_pubkey_type,
   $target_pool = 'zfsbackups',
-  $target_username = 'zfsbackup'
+  $target_username = 'zfsbackup',
+  $target_user_homedir = '/var/zfsbackup',
+  $target_user_uid = 9029,
 ) {
 
   # bring in common dependencies
   require zfsautosnap
 
+  $target_user_ensure = $::osfamily ? {
+    'Solaris' => 'role',
+    default   => 'present',
+  }
+
+  $target_user_shell = $::osfamily ? {
+    'Solaris' => '/bin/pfsh',
+    'FreeBSD' => '/usr/local/bin/bash',
+    default   => '/bin/bash',
+  }
+
+  user { $target_username:
+    ensure   => $target_user_ensure,
+    uid      => $target_user_uid,
+    comment  => 'ZFS Remote Backups Role',
+    password => '*NP*',
+    home     => $target_user_homedir,
+    shell    => $target_user_shell,
+  }->
+  file { "${target_user_homedir}":
+    ensure => 'directory',
+    owner  => $target_username,
+    group  => $target_username,
+    mode   => 0755,
+  }->
+  file { "${target_user_homedir}/.ssh":
+    ensure => 'directory',
+    owner  => $target_username,
+    group  => $target_username,
+    mode   => 0755,
+  }->
   # Install client key
   ssh_authorized_key { 'zfsautosnap client key':
     type    => $client_ssh_pubkey_type,
